417b1d8e22f465f228553eed471f3520b089ce5b
Kimmo Riihiaho <kimmo.riihiaho@gmail.com>
2023-12-01 16:03:39 +0200

    Pixel selection and multiple plots possible

--- src/__main__.py
+++ src/__main__.py
@@ -12,25 +12,41 @@ matplotlib.use("TkAgg")
 
 
 # GUI entity keys
-guiek_cube_dir = "-DIR-"
-guiek_file_list = "-FILE LIST-"
+guiek_cube_dir_selected = "-DIR-"
+# guiek_file_list = "-FILE LIST-"
+guiek_cube_false_color = "-FALSE COLOR-"
+
+
+"""
+Refreshing plot: https://gist.github.com/KenoLeon/e913de9e1fe690ebe287e6d1e54e3b97
+"""
+
+click_x = 0
+click_y = 0
+
+fig_px_plot = None
+cube_data = None
+fig_px_plot = plt.figure(figsize=(5, 4), dpi=100)
+ax_px_plot = fig_px_plot.add_subplot(111)
 
 
 file_list_column = [
     [
         sg.Text("Image Folder"),
-        sg.In(size=(25, 1), enable_events=True, key=guiek_cube_dir),
+        sg.In(size=(25, 1), enable_events=True, key=guiek_cube_dir_selected),
         sg.FolderBrowse(),
     ],
     [
-        sg.Listbox(values=[], enable_events=True, size=(80, 20), key=guiek_file_list)
+        # sg.Listbox(values=[], enable_events=True, size=(80, 20), key=guiek_file_list)
+        sg.Text("Cube false color"),
+        sg.Canvas(key=guiek_cube_false_color),
     ],
 ]
 
 image_viewer_column = [
     [sg.Text("Choose an ENVI cube directory from list on left:")],
     [sg.Canvas(key="-CANVAS-")],
-    [sg.Button("OK")]
+    [sg.Button("Clear")]
 ]
 
 layout = [
@@ -41,8 +57,6 @@ layout = [
     ]
 ]
 
-window = sg.Window("Cube Inspector", layout=layout, margins=(500,300), finalize=True)
-
 
 def draw_figure(canvas, figure):
     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
@@ -51,8 +65,36 @@ def draw_figure(canvas, figure):
     return figure_canvas_agg
 
 
-fig = None
-cube_data = None
+window = sg.Window("Cube Inspector", layout=layout, margins=(500,300), finalize=True)
+
+_VARS = {'window': False,
+         'fig_agg': draw_figure(window["-CANVAS-"].TKCanvas, fig_px_plot),
+         'pltFig': False}
+
+# _VARS['fig_agg'] = draw_figure(window["-CANVAS-"].TKCanvas, fig_px_plot)
+
+
+def mouse_event(eventi):
+    print('x: {} and y: {}'.format(eventi.xdata, eventi.ydata))
+
+    y = eventi.xdata
+    x = eventi.ydata
+
+    if x is not None and y is not None:
+        _VARS['fig_agg'].get_tk_widget().forget()
+        pixel = cube_data.read_pixel(row=int(x), col=int(y))
+        ax_px_plot.plot(pixel)
+        _VARS['fig_agg'] = draw_figure(window["-CANVAS-"].TKCanvas, fig_px_plot)
+    else:
+        print(f"Mouse click out of image area.")
+
+
+def clear_plot():
+    print(f"Clearing pixel plot")
+    _VARS['fig_agg'].get_tk_widget().forget()
+    ax_px_plot.clear()
+    _VARS['fig_agg'] = draw_figure(window["-CANVAS-"].TKCanvas, fig_px_plot)
+
 
 while True:
     event, values = window.read()
@@ -60,60 +102,49 @@ while True:
     if event == "Exit" or event == sg.WIN_CLOSED:
         break
 
-    # Folder name was filled in, make a list of files in the folder
-    if event == guiek_cube_dir:
+    if event == "Clear":
+        clear_plot()
 
-        cube_dir = values[guiek_cube_dir]
-        # print(f"Directory: {cube_dir}")
+    if event == guiek_cube_dir_selected:
 
-        try:
-            # Get list of files in folder
-            file_list = os.listdir(cube_dir)
-            # print(f"file_list: {file_list}")
+        cube_dir = values[guiek_cube_dir_selected]
 
-            hdr_found = False
-            raw_found = False
-            hdr_file_name = None
-            raw_file_name = None
+        hdr_found = False
+        raw_found = False
+        hdr_file_name = None
+        raw_file_name = None
 
-            cube_data = None
+        cube_data = None
 
-            for file_name in file_list:
-                if file_name.endswith(".hdr"):
-                    hdr_found = True
-                    hdr_file_name = file_name
-                if file_name.endswith(".raw"):
-                    raw_found = True
-                    raw_file_name = file_name
+        file_list = os.listdir(cube_dir)
+        for file_name in file_list:
+            if file_name.endswith(".hdr"):
+                hdr_found = True
+                hdr_file_name = file_name
+            if file_name.endswith(".raw"):
+                raw_found = True
+                raw_file_name = file_name
 
-            if hdr_found and raw_found:
-                print(f"Envi cube files OK. ")
-                hdr_path = os.path.join(cube_dir,hdr_file_name)
-                raw_path = os.path.join(cube_dir,raw_file_name)
-                cube_data = envi.open(file=hdr_path,image=raw_path)
+        if hdr_found and raw_found:
+            print(f"Envi cube files OK. ")
+            hdr_path = os.path.join(cube_dir,hdr_file_name)
+            raw_path = os.path.join(cube_dir,raw_file_name)
+            cube_data = envi.open(file=hdr_path,image=raw_path)
+            default_bands = [int(band) - 1 for band in cube_data.metadata['default bands']]
+            false_color_rgb = cube_data.read_bands(bands=default_bands)
 
-                # Add the plot to the window
-                # draw_figure(window["-CANVAS-"].TKCanvas, fig)
+            ######## False color canvas
+            fig_false_color = plt.figure(figsize=(5, 4), dpi=100)
+            ax_false_color = fig_false_color.add_subplot(111)
+            ax_false_color.imshow(false_color_rgb)
+            # Connect mouse click
+            cid = fig_false_color.canvas.mpl_connect('button_press_event', mouse_event)
 
-                print(f"Cube meta: {cube_data.metadata}")
-            else:
-                print(f"Not OK. Either hdr or raw file not found from given directory.")
-        except:
-            file_list = []
+            draw_figure(window[guiek_cube_false_color].TKCanvas, fig_false_color)
+        else:
+            print(f"Not OK. Either hdr or raw file not found from given directory.")
 
-        fnames = [
-            f for f in file_list
-            if os.path.isfile(os.path.join(cube_dir, f))
-        ]
 
-        window[guiek_file_list].update(fnames)
-
-        pixel = cube_data.read_pixel(row=int(cube_data.nrows / 2), col=int(cube_data.ncols / 2))
-
-        fig = plt.figure(figsize=(5, 4), dpi=100)
-        fig.add_subplot(111).plot(pixel)
-        # Add the plot to the window
-        draw_figure(window["-CANVAS-"].TKCanvas, fig)
 
 
 window.close()
